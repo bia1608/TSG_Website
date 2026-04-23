5b226cf39f2435211ba8ffbabed7b70890b91dbb
Bianca <dancibianca1@gmail.com>
2026-04-23 10:33:58 +0300

    BlogController modified

--- TSG_Website/Controllers/BlogController.cs
+++ TSG_Website/Controllers/BlogController.cs
@@ -1,12 +1,85 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TSG_Website.Data;
 
 namespace TSG_Website.Controllers
 {
-    public class BlogController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public BlogController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetArticles()
+        {
+            var blogs = await _context.Blogs.ToListAsync();
+            return Ok(blogs);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetArticle(int id)
+        {
+            var blog = await _context.Blogs.FindAsync(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            return Ok(blog);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateArticle([FromBody] Blog blog)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Blogs.Add(blog);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetArticle), new { id = blog.Id }, blog);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateArticle(int id, [FromBody] Blog updatedBlog)
+        {
+            if (id != updatedBlog.Id)
+            {
+                return BadRequest();
+            }
+
+            var blog = await _context.Blogs.FindAsync(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            blog.Title = updatedBlog.Title;
+            blog.Content = updatedBlog.Content;
+            blog.UpdatedAt = DateTime.UtcNow;
+            _context.Blogs.Update(blog);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArticle(int id)
+        {
+            var blog = await _context.Blogs.FindAsync(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            _context.Blogs.Remove(blog);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
